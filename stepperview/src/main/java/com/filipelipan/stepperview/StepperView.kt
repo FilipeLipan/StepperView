ca662b21d12edfadc5d35cff096aa8d08ade112a
Filipe <lispa9@hotmail.com>
2019-04-08 11:33:49 -0300

    add goToCheckedStep method

--- stepperview/src/main/java/com/filipelipan/stepperview/StepperView.kt
+++ stepperview/src/main/java/com/filipelipan/stepperview/StepperView.kt
@@ -26,6 +26,7 @@ class StepperView : ConstraintLayout {
     var count: Int = 2
     var currentStep: Int = 0
 
+    public var stepperClickListeners :StepperClickListeners? = null
 
     var entries: Array<CharSequence>? = null
 
@@ -52,7 +53,6 @@ class StepperView : ConstraintLayout {
         if (count < 1) {
             count = 1
         }
-
 
         createCompleteLine()
         createCheckViews()
@@ -94,6 +94,11 @@ class StepperView : ConstraintLayout {
                 constraintSet.connect(checkView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0)
             } else {
                 constraintSet.connect(checkView.id, ConstraintSet.START, checkViews.get(i - 1).id, ConstraintSet.END, 0)
+            }
+
+            checkView.setOnClickListener {
+                stepperClickListeners?.onStepClick(i + 1)
+                goToStep(i)
             }
 
             checkViews.add(checkView)
@@ -387,40 +392,13 @@ class StepperView : ConstraintLayout {
         recoverState(currentStep = currentStep)
     }
 
-    public fun goToStep(step: Int = 0){
-        //TODO add validations
-
-        currentStep = step;
-
-        for(i in 0..checkViews.size -1){
-            if(i <= currentStep - 1){
-                checkViews.get(i).checkButton()
-                checkViews.get(i).markAsFinished()
-            }else if(i == currentStep) {
-                checkViews.get(i).checkButton()
-            }else{
-                checkViews.get(i).unCheckButton()
-            }
-        }
-
-        if (currentStep > 0) {
-            val constraintSet = ConstraintSet()
-            constraintSet.clone(this)
-            constraintSet.connect(completeLineView.id, ConstraintSet.END, guidelines.get(currentStep - 1).id, ConstraintSet.END, 0)
-            constraintSet.applyTo(this)
-        }
-        if(currentStep > count){
-            currentStep = count;
-        }
-    }
-
     private fun recoverState(currentStep: Int = 0) {
 
         if (currentStep > 0) {
             val constraintSet = ConstraintSet()
             constraintSet.clone(this)
 
-            constraintSet.connect(completeLineView.id, ConstraintSet.END, checkViews.get(currentStep - 1).id, ConstraintSet.END, 0)
+            constraintSet.connect(completeLineView.id, ConstraintSet.END, checkViews.get(currentStep - 1).id, ConstraintSet.END, 5)
 
             constraintSet.applyTo(this)
         }
@@ -482,5 +460,63 @@ class StepperView : ConstraintLayout {
      */
     fun convertPixelsToDp(px: Float): Float {
         return px / (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+    }
+
+    interface StepperClickListeners{
+        fun onStepClick(position :Int)
+    }
+
+    fun goToStep(position :Int){
+
+        for(i in 0..position){
+            if(!checkViews.get(i).isChecked()){
+                checkViews.get(i).markAsFinished()
+            }
+        }
+
+        for(i in position + 1..checkViews.size - 1){
+            checkViews.get(i).unCheckButton()
+        }
+
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(this)
+
+        if(position > 0){
+            constraintSet.connect(completeLineView.id, ConstraintSet.END, guidelines.get(position).id, ConstraintSet.END, 5)
+        }else{
+            constraintSet.connect(completeLineView.id, ConstraintSet.END, guidelines[0].id, ConstraintSet.END,  0)
+            constraintSet.connect(completeLineView.id, ConstraintSet.START, guidelines[0].id, ConstraintSet.START, 0)
+        }
+
+        constraintSet.applyTo(this)
+
+        currentStep = position + 1
+    }
+
+    public fun goToCheckedStep(step: Int = 0){
+        //TODO add validations
+
+        currentStep = step;
+
+        for(i in 0..checkViews.size -1){
+            if(i <= currentStep - 1){
+                checkViews.get(i).checkButton()
+                checkViews.get(i).markAsFinished()
+            }else if(i == currentStep) {
+                checkViews.get(i).checkButton()
+            }else{
+                checkViews.get(i).unCheckButton()
+            }
+        }
+
+        if (currentStep > 0) {
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(this)
+            constraintSet.connect(completeLineView.id, ConstraintSet.END, guidelines.get(currentStep - 1).id, ConstraintSet.END, 0)
+            constraintSet.applyTo(this)
+        }
+        if(currentStep > count){
+            currentStep = count;
+        }
     }
 }
